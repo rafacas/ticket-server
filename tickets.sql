16150ce85f7e725c137ef2d8683590fb7c734531
Rafa Casado <rafacas@gmail.com>
2013-01-17 23:38:58 +0000

    Added SQL file to create Tickets table in MySQL

---
+++ tickets.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS `Tickets64`;
+CREATE TABLE `Tickets64` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `stub` char(1) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `stub` (`stub`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
